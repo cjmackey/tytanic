ed900992776844490d1d0a8de7971f67c54a52e5
carl mackey <carljmackey@gmail.com>
2015-09-12 21:37:04 -0400

    moving coverage

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-time ./node_modules/jshint/bin/jshint lib/ test/ && time ./node_modules/mocha/bin/mocha && time ./node_modules/mocha/bin/mocha --require blanket -R html-cov > coverage.html
+mkdir -p coverage
+
+time ./node_modules/jshint/bin/jshint lib/ test/ && time ./node_modules/mocha/bin/mocha && time ./node_modules/mocha/bin/mocha --require blanket -R html-cov > coverage/coverage.html || exit 1
