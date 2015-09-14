2b4c5316ff2ec8eb9185b0d7c9f8d96fb9ed5e93
carl mackey <carljmackey@gmail.com>
2015-09-13 20:49:00 -0400

    server sync

--- test/otSpec.js
+++ test/otSpec.js
@@ -23,8 +23,9 @@ describe('ot', function() {
       describe('create and delete', function() {
         it('creates and deletes', function() {
           var cot = new ot.CommonOT([]);
-          cot.applyOp(new ot.Op('create', null, ['blah', 'hello']));
-          expect(cot.database.blah).to.deep.equal({});
+          cot.applyOp(new ot.Op('create', null, ['blah', {a:1}]));
+          cot.applyOp(new ot.Op('create', null, ['hello']));
+          expect(cot.database.blah).to.deep.equal({a:1});
           expect(cot.database.hello).to.deep.equal({});
           cot.applyOp(new ot.Op('delete', null, ['blah']));
           expect(cot.database.blah).to.equal(undefined);
@@ -33,6 +34,7 @@ describe('ot', function() {
       });
 
       describe('altering op', function() {
+
         it('can alter objects', function() {
           var opDef = new ot.OpDef('example op', function(docs, args){
             docs[0].blah = args[0];
@@ -43,6 +45,10 @@ describe('ot', function() {
           expect(cot.database.test.blah).to.equal('warglebargle');
           cot.applyOp(new ot.Op('example op', ['test'], ['asdf']));
           expect(cot.database.test.blah).to.equal('asdf');
+        });
+
+        it('does nothing if an object is missing (e.g. was deleted)', function() {
+
         });
       });
 
@@ -97,8 +103,55 @@ describe('ot', function() {
       expect(cot.canonDatabase.test.k1).to.equal('v1');
       expect(cot.canonDatabase.test.k2).to.equal('v2');
       expect(cot.log).to.deep.equal([]);
+      cot.receiveOps([new ot.Op('create', null, ['test2', {a:3}])]);
+      expect(cot.database.test2).to.deep.equal({a:3});
+    });
+
+    it('sends operations to the server', function() {
+      var sentOps = [];
+      var sendFunc = function(op) {
+        sentOps.push(op);
+      };
+      var cot = new ot.ClientOT([], 'myid', sendFunc);
+      cot.run('create', null, ['test']);
+      cot.run('create', null, ['test2']);
+      expect(sentOps).to.deep.equal(cot.log);
     });
 
   });
 
+
+  describe('ServerOT', function() {
+    var opDef = new ot.OpDef('example op', function(docs, args){
+      docs[0].blah = args[0];
+    });
+    var sot, cot1, cot2;
+    var connect = function(cot, sot) {
+      sot.addClient(cot.clientId, function(op) {
+        cot.receiveOps([op]);
+      });
+    };
+    beforeEach(function() {
+      sot = new ot.ServerOT([opDef]);
+      cot1 = new ot.ClientOT([opDef], 'id1', function(op) { sot.recvOp(op); });
+      cot2 = new ot.ClientOT([opDef], 'id2', function(op) { sot.recvOp(op); });
+      connect(cot1, sot);
+      connect(cot2, sot);
+    });
+
+    it('syncs!', function() {
+      cot1.run('create', null, ['test', {a:1}]);
+      expect(cot1.database.test).to.deep.equal({a:1});
+      expect(cot2.database.test).to.equal(undefined);
+      expect(cot1.log).to.deep.equal([]);
+      cot2.subscribe(['test']);
+      expect(cot2.database.test).to.deep.equal({a:1});
+      cot1.run('example op', ['test'], ['hello']);
+      expect(cot1.database.test.blah).to.equal('hello');
+      expect(cot2.database.test.blah).to.equal('hello');
+      cot2.run('example op', ['test'], ['asdf']);
+      expect(cot2.database.test.blah).to.equal('asdf');
+      expect(cot1.database.test.blah).to.equal('asdf');
+    });
+  });
 });
