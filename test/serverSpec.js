6da88bb9ddb026ade3b261adccbc275a06cefc4e
Carl Mackey <carl@styleseat.com>
2015-10-13 15:03:35 -0700

    ...

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -39,6 +39,67 @@ describe('server', function() {
       server.initDB(sqliteDbUrl).then(done);
     });
 
+    describe('getSnap', function() {
+
+      it('returns an empty snap if there are none', function(done) {
+        myServer.getSnap(randomId).then(function(snap) {
+          expect(snap.opId).to.equal(null);
+          expect(snap.data).to.deep.equal({});
+          done();
+        });
+      });
+
+      it('returns the latest snap if there is one', function(done) {
+        var snap = {objectId: randomId, opId: 1, data: JSON.stringify({a: 1})};
+        server.models.Snap.create(snap).then(function() {
+          snap = {objectId: randomId, opId: 1, data: JSON.stringify({a: 2})};
+          server.models.Snap.create(snap).then(function() {
+            myServer.getSnap(randomId).then(function(snap) {
+              console.log(snap);
+              expect(snap.opId).to.equal(1);
+              expect(snap.data).to.deep.equal({a: 2});
+              done();
+            });
+          });
+        });
+      });
+
+    });
+
+    describe('createSnap', function() {
+
+      it('if there is nothing, it creates an empty snapshot', function(done) {
+        myServer.createSnap(randomId).then(function(snap) {
+          expect(snap.data).to.deep.equal({});
+          expect(snap.opId).to.equal(null);
+          done();
+        });
+      });
+
+      // TODO: add more tests!
+      // if there are ops, but no snaps
+      // if there is a snap, but no ops
+      // if there are both snaps and ops
+
+    });
+
+    describe('getSnapAndLogs', function() {
+
+      it('gets ops in the case where there is no snapshot', function(done) {
+        server.models.Op.create({objectId: randomId, opName: 'asdf'}).then(function() {
+          myServer.getSnapAndLogs(randomId).then(function(so) {
+            var snap = so[0];
+            var ops = so[1];
+            expect(ops.length).to.equal(1);
+            expect(ops[0].opName.toString()).to.equal('asdf');
+            done();
+          });
+        });
+      });
+
+      // TODO: create a snapshot, then do it
+    });
+
     describe('receiveMessage', function() {
 
       it('receives an op message', function(done) {
@@ -55,7 +116,7 @@ describe('server', function() {
         });
       });
 
-      it('receives a subscribe message', function(done) {
+      it('receives a subscribe message, adds the connection to the list of subscribers, and sends back the recent snap and logs', function(done) {
         myServer.receiveMessage({
           messageType: 'subscribe',
           objectId: randomId,
@@ -63,6 +124,15 @@ describe('server', function() {
           expect(myServer.subscriptions[randomId].subscribers[conn.id]).to.equal(conn);
           expect(myServer.connectionSubs[conn.id][randomId]).to.equal(true);
           expect(myServer.subRedis.subscribe.calledWith('tytanic.ops.' + randomId)).to.equal(true);
+          console.log(conn.write.args);
+          expect(conn.write.calledWith(JSON.stringify({
+            messageType: 'snap',
+            snap: {
+              objectId:randomId,
+              opId: null,
+              data:{}
+            },
+          }))).to.equal(true);
           done();
         });
       });
